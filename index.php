0bb3bb025d38b38d174d5383f43b60a57cc14299
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-02-08 20:27:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,16 @@
 echo "<h1>PHP - Aula 02 </h1>";
 echo "<h2>Trabalhando com operadores</h2>";
 echo"<hr>";
-echo "Daniel e gostuso";
+
+$nome = "Daniel";
+$idade = "40";
+$altura = 1.65;
+$fumante = true;
+
+echo $nome;
+echo "br";
+echo $idade;
+echo "br";
+echo $fumante;
+
 ?>
