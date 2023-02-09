7099783f591833cb17d1180d059e5c0c23536d46
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-02-08 21:23:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,39 @@ echo "<h1>PHP - Aula 02 </h1>";
 echo "<h2>Trabalhando com operadores</h2>";
 echo"<hr>";
 
-$nome = "Daniel";
+$nome = "Daniel Santos";
 $idade = "40";
 $altura = 1.65;
 $fumante = true;
 
-echo $nome;
+echo "Nome :" .$nome;
 echo "<br>";
-echo $idade;
+echo  "Idade :" .  $idade;
 echo "<br>";
-echo $fumante;
+echo "Altura :" . $altura;
+echo "<br>";
+echo "Fumante :" . $fumante;
+echo "<br>";
+
+echo $nome . " - ". $idade ." - ". $altura . " - ". $fumante;
+
+echo "<hr>";
+
+echo gettype($nome) . "<br>";
+echo gettype($altura) . "<br>";
+echo gettype($idade) . "<br>";
+echo gettype($fumante) . "<br>";
+
+
+echo "<h1>Operadores aritmético </h1>";
+ $n1 = 10;
+ $n2 = 2;
+
+ echo $n1 + $n2 . "<br>";
+ echo $n1 - $n2 . "<br>";
+ echo $n1 * $n2 . "<br>";
+ echo $n1 / $n2 . "<br>";
+ echo $n1 % 2 .   "<br>";
+ echo $n1 ** 2 .  "<br>";
 
 ?>
